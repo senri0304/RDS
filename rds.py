3d535a2180e86ecb653b1c020133fe9d36ccac2c
hasegawa <43361198+senri0304@users.noreply.github.com>
2019-09-09 14:42:01 +0900

    Update rds.py
    update to save a lot

--- rds.py
+++ rds.py
@@ -1,48 +1,54 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[92]:
-
-
+import os
 import numpy as np
 from PIL import Image, ImageDraw
+
+to_dir = 'material' 
+os.makedirs(to_dir, exist_ok=True)
 
 # Input RDS's size, caring be dividable 
 size = 100
 
 # Input the disparity, 
-disparity = 4
+disparity = 6
 
 # The target size is half of RDS’s　size in default 
 inner = int(size/ 2)
 
-# Two images prepair
-img = Image.new("L", (size, size), 255)
-draw = ImageDraw.Draw(img)
+# Input the quantity you need
+q = 10
+for k in range(1, q):
 
-img2 = Image.new("L", (size, size), 255)
-draw2 = ImageDraw.Draw(img2)
-
-# Drawing the planes of RDS
-for i in range(0, size):
-    for j in range(1, size + 1):
-        x = np.round(np.random.binomial(1, 0.5, 1))*(j)
-        draw.point((x, i), fill=(0))
-        draw2.point((x, i), fill=(0))
-
-# Filling the targets area
-draw.rectangle((int(size/2) - int(inner/2)-1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)-1, int(size/2) - int(inner/2)), fill=255, outline=None)
-draw2.rectangle((int(size/2) - int(inner/2) + disparity -1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)- 1+ disparity, (size/2) - (inner/2)), fill=255, outline=None)
-
-# Drawing the targets
-for i in range(0, inner+1):
-    for j in range(0, inner + 1):
-        x = np.round(np.random.binomial(1, 0.5, 1))*(1+j)
-        if x != 0:
-            draw.point((x + ((size/2) - (inner/2)) -2 , i + (size/2) - (inner/2)))
-            draw2.point((x + (size/2) - (inner/2) -2 + disparity, i + (size/2) - (inner/2)))
-
-# Write images
-img.save('rds.jpg', quality=100)
-img2.save('rds2.jpg', quality=100)
-
+    # Two images prepair
+    img = Image.new("L", (size, size), 255)
+    draw = ImageDraw.Draw(img)
+    
+    img2 = Image.new("L", (size, size), 255)
+    draw2 = ImageDraw.Draw(img2)
+    
+    # Drawing the planes of RDS
+    for i in range(0, size):
+        for j in range(1, size + 1):
+            x = np.round(np.random.binomial(1, 0.5, 1))*(j)
+            draw.point((x, i), fill=(0))
+            draw2.point((x, i), fill=(0))
+    
+    # Filling the targets area
+    draw.rectangle((int(size/2) - int(inner/2)-1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)-1, int(size/2) - int(inner/2)), fill=255, outline=None)
+    draw2.rectangle((int(size/2) - int(inner/2) + disparity -1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)- 1+ disparity, (size/2) - (inner/2)), fill=255, outline=None)
+    
+    # Drawing the targets
+    for i in range(0, inner+1):
+        for j in range(0, inner + 1):
+            x = np.round(np.random.binomial(1, 0.5, 1))*(1+j)
+            if x != 0:
+                draw.point((x + ((size/2) - (inner/2)) -2 , i + (size/2) - (inner/2)))
+                draw2.point((x + (size/2) - (inner/2) -2 + disparity, i + (size/2) - (inner/2)))
+    
+    # Write images
+    basenameR = os.path.basename('rds' + str(k) + 'R.jpg')
+    basenameL = os.path.basename('rds' + str(k) + 'L.jpg')
+    img.save(os.path.join(to_dir, basenameR), quality=100)
+    img2.save(os.path.join(to_dir, basenameL), quality=100)
