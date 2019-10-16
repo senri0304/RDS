89d60c30c528c966dc48c4e4f8352d0606a679c6
hasegawa <43361198+senri0304@users.noreply.github.com>
2019-10-16 18:04:35 +0900

    Update rds.py

--- rds.py
+++ rds.py
@@ -11,15 +11,20 @@ os.makedirs(to_dir, exist_ok=True)
 # Input RDS's size, caring be dividable 
 size = 100
 
-# Input the disparity, 
+# Input the disparity at pixel units
 disparity = 6
 
-# The target size is half of RDS’s　size in default 
+# Input target size, which is half of RDS’s　size in default 
 inner = int(size/ 2)
 
 # Input the quantity you need
-q = 10
-for k in range(1, q):
+q = 5
+
+# Input a number you like to initiate
+s = 0
+
+# Generate RDSs
+for k in range(s, s + q):
 
     # Two images prepair
     img = Image.new("L", (size, size), 255)
@@ -28,24 +33,24 @@ for k in range(1, q):
     img2 = Image.new("L", (size, size), 255)
     draw2 = ImageDraw.Draw(img2)
     
-    # Drawing the planes of RDS
+    # Draw the planes of RDSs
     for i in range(0, size):
         for j in range(1, size + 1):
             x = np.round(np.random.binomial(1, 0.5, 1))*(j)
             draw.point((x, i), fill=(0))
             draw2.point((x, i), fill=(0))
     
-    # Filling the targets area
-    draw.rectangle((int(size/2) - int(inner/2)-1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)-1, int(size/2) - int(inner/2)), fill=255, outline=None)
-    draw2.rectangle((int(size/2) - int(inner/2) + disparity -1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2)- 1+ disparity, (size/2) - (inner/2)), fill=255, outline=None)
+    # Fill the targets area
+    draw.rectangle((int(size/2) - int(inner/2) - disparity/2 - 1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2) - 1 - disparity/2, int(size/2) - int(inner/2)), fill=255, outline=None)
+    draw2.rectangle((int(size/2) - int(inner/2) + disparity/2 - 1,  int(size/2) + int(inner/2), int(size/2) + int(inner/2) - 1 + disparity/2, int(size/2) - int(inner/2)), fill=255, outline=None)
     
     # Drawing the targets
     for i in range(0, inner+1):
         for j in range(0, inner + 1):
             x = np.round(np.random.binomial(1, 0.5, 1))*(1+j)
             if x != 0:
-                draw.point((x + ((size/2) - (inner/2)) -2 , i + (size/2) - (inner/2)))
-                draw2.point((x + (size/2) - (inner/2) -2 + disparity, i + (size/2) - (inner/2)))
+                draw.point((x + ((size/2) - (inner/2)) -2 - disparity/2, i + (size/2) - (inner/2)))
+                draw2.point((x + (size/2) - (inner/2) -2 + disparity/2, i + (size/2) - (inner/2)))
     
     # Write images
     basenameR = os.path.basename('rds' + str(k) + 'R.jpg')
